1cd2e386941aac0a23c249ed5c9c42c0413c2a0d
fantasynovel <35621704+fantasynovel@users.noreply.github.com>
2018-08-26 11:46:19 +0800

    透明的 navbar

---
+++ navbar&footer_php/index_ nav_transparent.php
@@ -0,0 +1,4 @@
+<?php include __DIR__. '/__html_head.php' ?>
+<?php include __DIR__. '/__navbar_transparent.php' ?>
+<?php include __DIR__. '/__footer.php' ?>
+<?php include __DIR__. '/__html_foot.php' ?>
