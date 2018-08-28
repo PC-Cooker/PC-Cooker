e45ae3984708b979c5f753f2b09b21db01e9bab1
hc5705iiiorg <42430561+hc5705iiiorg@users.noreply.github.com>
2018-08-28 18:23:14 +0800

    Add files via upload

--- pcphp/cart.php
+++ pcphp/cart.php
@@ -13,56 +13,10 @@ if (!empty($_SESSION['cart'])) {
 }
 ?>
 <?php include __DIR__ . '/__html_head.php' ?>
-<link rel="stylesheet" href="comment.css">
 <link rel="stylesheet" href="cart.css">
-<div class="container">
-    <?php include __DIR__ . '/__navbar.php' ?>
-    <?php if (!empty($_SESSION['cart'])): ?>
-    <?php endif; ?>
-</div>
-<div class="container-fluid">
-    <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#"><img src="img/logo_big.svg"></a>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item pr-4">
-                    <a class="nav-link" href="#">最新活動</a>
-                </li>
-                <li class="nav-item pr-4">
-                    <a class="nav-link" href="#">我要組電腦</a>
-                </li>
-                <li class="nav-item pr-4">
-                    <a class="nav-link" href="product_list.php">我要買零件</a>
-                </li>
-                <li class="nav-item pr-4">
-                    <a class="nav-link" href="#">用戶評論區</a>
-                </li>
-                <li class="nav-item pr-4">
-                    <a class="nav-link" href="#">科普小遊戲</a>
-                </li>
-            </ul>
-            <button type="login" class="login"><img src="img/member.svg"></button>
-            <button type="cart" class="cart"><img src="img/shopping-cart.svg"></button>
-            <!-- toggle-switch start -->
-            <div class="toggle-switch d-flex ml-3">
-                <label class="switch">
-                    <input type="checkbox">
-                    <span class="slider round"></span>
-                </label>
-            </div>
-            <!-- toggle-switch end -->
-        </div>
-    </nav>
-</div>
-<!-- end of navbar -->
-
-
-<!-- bs4 的 雷，Navbar 設 fixed-top 下面文字會被遮住，下面是解法 -->
-<div class="container-fluid blue">
-    <div class="fixed-top-space"></div>
-</div>
-<button></button>
-<!-- cart -->
+</head>
+<body>
+<?php include __DIR__ . '/__navbar.php' ?>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -291,90 +245,9 @@ if (!empty($_SESSION['cart'])) {
         </div>
     </div>
 </div>
-
-<!-- footer -->
-<div class="container-fluid black">
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-sm-4 d-none d-sm-block">
-                    <img src="img/logo_big.svg" class="f-logo">
-                </div>
-                <div class="col-12 col-sm-8 d-none d-sm-block">
-                    <nav class="nav">
-                        <ul class="nav justify-content-center">
-                            <li class="nav-item"><a class="nav-link f-title" href="#">顧客服務</a>
-                                <ul>
-                                    <li><a class="nav-link" href="#">配件/軟體加購</a></li>
-                                    <li><a class="nav-link" href="#">保固方式</a></li>
-                                    <li><a class="nav-link" href="#">送貨資訊</a></li>
-                                    <li><a class="nav-link" href="#">退換貨方式</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item"><a class="nav-link f-title" href="#">支援服務</a>
-                                <ul>
-                                    <li><a class="nav-link" href="#">網站導覽</a></li>
-                                    <li><a class="nav-link" href="#">聯絡我們</a></li>
-                                    <li><a class="nav-link" href="#">線上客服</a></li>
-                                    <li><a class="nav-link" href="#">隱私權條款</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item"><a class="nav-link f-title" href="#">關於我們</a>
-                                <ul>
-                                    <li><a class="nav-link" href="#">關於酷客</a></li>
-                                    <li><a class="nav-link" href="#">最新活動</a></li>
-                                    <li><a class="nav-link" href="#">我要組電腦</a></li>
-                                    <li><a class="nav-link" href="#">我要買零件</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <!-- 手機 footer 手風琴 -->
-        <div class="container d-md-none">
-            <div class="tab">
-                <input id="tab-one" type="checkbox" name="tabs" class="f-tab">
-                <label for="tab-one" class="f-tab">顧客服務</label>
-                <div class="tab-content">
-                    <ul>
-                        <li><a class="" href="#">配件/軟體加購</a></li>
-                        <li><a class="" href="#">保固方式</a></li>
-                        <li><a class="" href="#">送貨資訊</a></li>
-                        <li><a class="" href="#">退換貨方式</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="tab">
-                <input id="tab-two" type="checkbox" name="tabs" class="f-tab">
-                <label for="tab-two" class="f-tab">支援服務</label>
-                <div class="tab-content">
-                    <ul>
-                        <li><a class="" href="#">網站導覽</a></li>
-                        <li><a class="" href="#">聯絡我們</a></li>
-                        <li><a class="" href="#">線上客服</a></li>
-                        <li><a class="" href="#">隱私權條款</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="tab">
-                <input id="tab-three" type="checkbox" name="tabs" class="f-tab">
-                <label for="tab-three" class="f-tab">關於我們</label>
-                <div class="tab-content">
-                    <ul>
-                        <li><a class="" href="#">關於酷客</a></li>
-                        <li><a class="" href="#">最新活動</a></li>
-                        <li><a class="" href="#">我要組電腦</a></li>
-                        <li><a class="" href="#">我要買零件</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <p class="py-3">Copyright © 2018 PC COOKER All rights reserved.</p>
-    </footer>
 </div>
-
+<!-- footer -->
+<?php include __DIR__ . '/__footer.php' ?>
 <!-- 選擇性的 JavaScript -->
 <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"
         integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy"
